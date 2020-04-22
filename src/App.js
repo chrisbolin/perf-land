5e7ea26e32bacf5fe3ba2171cae9faae7f6201fa
Chris Bolin <bolin.chris@gmail.com>
2020-04-22 17:37:14 -0600

    async search

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { parse } from "papaparse";
 import { groupBy } from "lodash";
-import Select from "react-select";
+import AsyncSelect from "react-select/async";
 
 import "./App.css";
 
@@ -20,24 +20,41 @@ function downloadRecords() {
   });
 }
 
+function getUrlMatches(searchString, urls) {
+  const MAX_RESULTS = 100;
+  return urls.filter((url) => url.includes(searchString)).slice(0, MAX_RESULTS);
+}
+
 function App() {
   const [records, setRecords] = useState({});
   const [urls, setUrls] = useState([]);
-  const [urlOptions, setUrlOptions] = useState([]);
   useEffect(() => {
     downloadRecords().then((records) => {
       setRecords(records);
       const urls = Object.keys(records);
       setUrls(urls);
-      const options = urls.map((url) => ({ value: url, label: url }));
-      setUrlOptions(options);
     });
   }, []);
   window.records = records;
+
+  const loadOptions = (inputValue, callback) => {
+    const MIN_INPUT = 3;
+    if (inputValue.length < MIN_INPUT) {
+      callback([]);
+    } else {
+      callback(
+        getUrlMatches(inputValue, urls).map((url) => ({
+          value: url,
+          label: url,
+        }))
+      );
+    }
+  };
+
   return (
     <div className="App">
       <h2>{urls.length}</h2>
-      <Select options={urlOptions} />
+      <AsyncSelect loadOptions={loadOptions} defaultOptions={[]} />
     </div>
   );
 }
