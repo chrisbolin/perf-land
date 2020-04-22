f5ac391840a22952a00d8836f7e71a4a33d5b451
Chris Bolin <bolin.chris@gmail.com>
2020-04-22 17:10:59 -0600

    all options in Select

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { parse } from "papaparse";
 import { groupBy } from "lodash";
+import Select from "react-select";
+
 import "./App.css";
 
 function downloadRecords() {
@@ -11,7 +13,7 @@ function downloadRecords() {
       header: true,
       complete: (results) => {
         console.timeEnd("download");
-        resolve(groupBy(results.data, (rec) => rec.url + "!!!"));
+        resolve(groupBy(results.data, "url"));
       },
       error: (error) => reject(error),
     });
@@ -20,17 +22,22 @@ function downloadRecords() {
 
 function App() {
   const [records, setRecords] = useState({});
-  const [urls, setURLs] = useState([]);
+  const [urls, setUrls] = useState([]);
+  const [urlOptions, setUrlOptions] = useState([]);
   useEffect(() => {
     downloadRecords().then((records) => {
       setRecords(records);
-      setURLs(Object.keys(records));
+      const urls = Object.keys(records);
+      setUrls(urls);
+      const options = urls.map((url) => ({ value: url, label: url }));
+      setUrlOptions(options);
     });
   }, []);
   window.records = records;
   return (
     <div className="App">
       <h2>{urls.length}</h2>
+      <Select options={urlOptions} />
     </div>
   );
 }
