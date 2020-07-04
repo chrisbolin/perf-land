ab1d30fe6407e0a8b8f4f7dfce98cad7a54dde97
Chris Bolin <bolin.chris@gmail.com>
2020-07-04 11:23:09 -0600

    move out bucket searching functionality

--- src/api.test.ts
+++ src/api.test.ts
@@ -1,10 +1,4 @@
-import {
-  urlToId,
-  urlToStorageUrl,
-  objectsApiUrl,
-  searchPrefixes,
-  removePath,
-} from "./api";
+import { urlToId, urlToStorageUrl } from "./api";
 
 const AOL_URL = "https://www.aol.com/";
 
@@ -20,53 +14,5 @@ describe("urlToStorageUrl", () => {
     const aolStorageUrl =
       "https://storage.googleapis.com/perf-land/sites/011/https:www.aol.com.json";
     expect(urlToStorageUrl(AOL_URL)).toBe(aolStorageUrl);
-  });
-});
-
-describe("objectsApiUrl", () => {
-  it("creates storage url for listing all objects with prefix", () => {
-    const prefix = "https:aol";
-    const listUrl =
-      "https://storage.googleapis.com/storage/v1/b/perf-land/o/?prefix=sites%2F011%2Fhttps%3Aaol&maxResults=20";
-    expect(objectsApiUrl(prefix)).toBe(listUrl);
-  });
-});
-
-describe("removePath", () => {
-  it("doesn't change urls without paths", () => {
-    const url = "https://www.aol.com";
-    expect(removePath(url)).toBe(url);
-  });
-
-  it("removes paths", () => {
-    const url = "https://www.aol.com/about";
-    const urlWithoutPath = "https://www.aol.com";
-    expect(removePath(url)).toBe(urlWithoutPath);
-  });
-
-  it("removes trailing slash", () => {
-    const url = "https://www.aol.com/";
-    const urlWithoutPath = "https://www.aol.com";
-    expect(removePath(url)).toBe(urlWithoutPath);
-  });
-});
-
-describe("searchPrefixes", () => {
-  it("generates one prefix for fully prefixed searches", () => {
-    const search = "https://aol";
-    const prefixes = [search];
-    expect(searchPrefixes(search)).toEqual(prefixes);
-  });
-
-  it("generates http and https prefixes for un-qualified searches", () => {
-    const search = "aol";
-    const prefixes = ["http://aol", "https://aol"];
-    expect(searchPrefixes(search)).toEqual(prefixes);
-  });
-
-  it("removes any path", () => {
-    const search = "aol.com/about/";
-    const prefixes = ["http://aol.com", "https://aol.com"];
-    expect(searchPrefixes(search)).toEqual(prefixes);
   });
 });
--- src/api.ts
+++ src/api.ts
@@ -2,8 +2,6 @@ const ROOT_URL = `https://storage.googleapis.com`;
 const BUCKET_NAME = "perf-land";
 const SITES_PATH = "sites/011";
 
-const OBJECTS_API_URL = `${ROOT_URL}/storage/v1/b/${BUCKET_NAME}/o/`;
-
 export function urlToId(url: string): string {
   return url.replace(/\//g, "");
 }
@@ -19,26 +17,4 @@ export function urlToStorageUrl(url: string): string {
     urlToId(url) +
     ".json"
   );
-}
-
-export function objectsApiUrl(search: string, maxResults = 20): string {
-  const fullPrefix = encodeURIComponent(SITES_PATH + "/" + search);
-  return `${OBJECTS_API_URL}?prefix=${fullPrefix}&maxResults=${maxResults}`;
-}
-
-export function removePath(url: string): string {
-  const match = url.match(/[a-zA-Z0-9]\//);
-  if (match && match.index) {
-    return url.substring(0, match.index + 1);
-  }
-  return url;
-}
-
-export function searchPrefixes(search: string): string[] {
-  let correctedSearch = removePath(search);
-
-  // search already prefixes protocol, just return it
-  if (correctedSearch.startsWith("http")) return [correctedSearch];
-
-  return ["http://" + correctedSearch, "https://" + correctedSearch];
 }
