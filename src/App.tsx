2404c57c1fcbab3aae0b3eaeb3011a190b3b0f25
Paula Lavalle <lavalle.paula@gmail.com>
2020-07-05 22:51:25 -0700

    Add SiteDetailsTable component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,7 @@
-import React, { useReducer, memo } from "react";
-import {
-  VictoryBar,
-  VictoryBrushContainer,
-  VictoryChart,
-  VictoryAxis,
-  VictoryLabel,
-} from "victory";
+import React, { useReducer } from "react";
 import styled from "styled-components/macro";
 
 import {
-  AugmentedSite,
   PresetName,
   reducer,
   initializeState,
@@ -22,10 +14,12 @@ import {
 
 import ActiveSiteList from "./components/ActiveSiteList";
 import Button from "./components/Button";
+import Chart from "./components/Chart";
 import Collections from "./components/Collections";
 import Heading from "./components/Heading";
 import Hero from "./components/Hero";
 import Sidebar from "./components/Sidebar";
+import SiteDetailsTable from "./components/SiteDetailsTable";
 import StyledSelect from "./components/StyledSelect";
 
 import "./App.css";
@@ -36,136 +30,9 @@ declare global {
   }
 }
 
-type EventCallbackFunction = (event: React.SyntheticEvent) => void;
-type IdentityFunction = (x: number) => number;
-
 function LoadingSites() {
-  return <span>loading...</span>;
+  return <div>loading...</div>;
 }
-
-function SiteDetail({ site }: { site: AugmentedSite }) {
-  return (
-    <tr>
-      <td>
-        <a href={site.url}>{site.name}</a>
-      </td>
-      <td>{site.cdn || "none detected"}</td>
-      <td>
-        {new Date(site.startedDateTime * 1000).toLocaleString(undefined, {
-          timeZoneName: "short",
-        })}
-      </td>
-    </tr>
-  );
-}
-function ChartNoMemo({
-  sites,
-  field,
-  name,
-  highlightedUrl,
-  reverse = false,
-  yTransform = (x) => x,
-}: {
-  sites: AugmentedSite[];
-  field:
-    | "TTFB"
-    | "firstContentfulPaint"
-    | "firstMeaningfulPaint"
-    | "firstCPUIdle"
-    | "timeToInteractive"
-    | "maxPotentialFirstInputDelay"
-    | "speedIndex"
-    | "performanceScore"
-    | "bytesJS"
-    | "bytesImg"
-    | "bytesTotal"
-    | "reqTotal";
-
-  name: string;
-  highlightedUrl: string;
-  reverse?: boolean;
-  yTransform?: IdentityFunction;
-}) {
-  if (!sites.length) return null;
-
-  const data = sites
-    .map((site) => ({
-      ...site,
-      isHighlighted: site.url === highlightedUrl,
-      x: site.name,
-      y: yTransform(site[field]) || 0,
-    }))
-    // standard sort: ascending; reverse: descending
-    .sort((a, b) => (a.y - b.y) * (reverse ? 1 : -1));
-
-  const brushDomainStart = data.findIndex((site) => site.isHighlighted);
-
-  return (
-    <div className="Chart">
-      <span className="Chart-title">{name}</span>
-      <VictoryChart
-        width={360}
-        height={data.length * 60}
-        domainPadding={20}
-        padding={{ top: 20, right: 20, bottom: 50, left: 1 }}
-        containerComponent={
-          <VictoryBrushContainer
-            allowDrag={false}
-            allowResize={false}
-            brushDimension="x"
-            brushDomain={{
-              x:
-                brushDomainStart >= 0
-                  ? [brushDomainStart + 0.6, brushDomainStart + 1.75]
-                  : [0, 0],
-            }}
-            brushStyle={{
-              fill: "#feeb5c",
-              fillOpacity: 0.1,
-            }}
-          />
-        }
-      >
-        <VictoryBar
-          horizontal
-          data={data}
-          barWidth={10}
-          style={{
-            data: {
-              fill: ({ datum }) => datum.color,
-            },
-          }}
-        />
-        <VictoryAxis
-          dependentAxis
-          style={{
-            axis: { stroke: "#000", strokeWidth: 1 },
-            grid: { stroke: "#fff" },
-          }}
-        />
-        <VictoryAxis
-          style={{
-            axis: { stroke: "#ff0000", strokeWidth: 0 },
-          }}
-          tickFormat={(t, index) => {
-            const isHighlighted = data[index].isHighlighted;
-            return isHighlighted ? `${t} ⭐️` : `${t}`;
-          }}
-          tickLabelComponent={
-            <VictoryLabel textAnchor="start" dy={-15} dx={14} />
-          }
-        />
-      </VictoryChart>
-    </div>
-  );
-}
-
-const Chart = memo(
-  ChartNoMemo,
-  (prevProps, nextProps) =>
-    prevProps.highlightedUrl === nextProps.highlightedUrl &&
-    prevProps.sites.length === nextProps.sites.length
-);
 
 const Wrapper = styled.div`
   padding: ${(props) => props.theme.spacing(10)}
@@ -179,11 +46,22 @@ const Layout = styled.div`
 
   margin-top: ${(props) => props.theme.spacing(16)};
   padding-top: ${(props) => props.theme.spacing(4)};
+  padding-bottom: ${(props) => props.theme.spacing(8)};
 
   background-color: ${(props) => props.theme.colors.lightTan};
 `;
 
-const Content = styled.div``;
+const Content = styled.div`
+  flex: 1 1 auto;
+`;
+
+const ContentHeader = styled.div`
+  align-items: flex-end;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-between;
+`;
 
 function App() {
   const [state, dispatch] = useReducer(reducer, undefined, initializeState);
@@ -231,6 +109,7 @@ function App() {
             onClick={(name: string) => dispatch(actions.selectCollection(name))}
             activeSites={
               <React.Fragment>
+                {hasUrls && loadingSites ? <LoadingSites /> : null}
                 <ActiveSiteList
                   hasUrls={hasUrls}
                   highlightedUrl={highlightedUrl}
@@ -274,19 +153,23 @@ function App() {
             }
           />
           {viewingSavedCollection ? (
-            <Button onClick={() => promptAndSaveCollection()}>
-              update "{state.currentCollection.name}"
-            </Button>
+            <React.Fragment>
+              <br />
+              <Button onClick={() => promptAndSaveCollection()}>
+                update "{state.currentCollection.name}"
+              </Button>
+              <br />
+              <Button
+                onClick={() =>
+                  dispatch(
+                    actions.deleteCollection(state.currentCollection.name)
+                  )
+                }
+              >
+                delete "{state.currentCollection.name}"
+              </Button>
+            </React.Fragment>
           ) : null}
-          {viewingSavedCollection && (
-            <Button
-              onClick={() =>
-                dispatch(actions.deleteCollection(state.currentCollection.name))
-              }
-            >
-              delete
-            </Button>
-          )}
           <Heading as="h3" size="small">
             Examples
           </Heading>
@@ -298,6 +181,7 @@ function App() {
             }
             activeSites={
               <React.Fragment>
+                {hasUrls && loadingSites ? <LoadingSites /> : null}
                 <ActiveSiteList
                   hasUrls={hasUrls}
                   highlightedUrl={highlightedUrl}
@@ -343,22 +227,29 @@ function App() {
         </Sidebar>
 
         <Content>
-          <Heading as="h2" size="large">
-            {state.currentCollection.name} {/* TODO: If modified, add '*' */}
-          </Heading>
-          {hasUrls ? (
-            <div className="Collections-Cell Collections-Cell--right">
-              {loadingSites && <LoadingSites />}
-              <Button onClick={() => dispatch(actions.clearAllSelectedUrls())}>
-                Clear All
-              </Button>
-              {viewingSavedCollection ? null : (
-                <Button onClick={() => promptAndSaveCollection()}>
-                  Save to My Stuff
+          <ContentHeader>
+            <Heading as="h2" size="large">
+              {state.currentCollection.name
+                ? state.currentCollection.name
+                : "[click something on the left]"}{" "}
+              {/* TODO: If it's modified & unsaved, add '*' */}
+            </Heading>
+            {hasUrls ? (
+              <div>
+                <Button
+                  onClick={() => dispatch(actions.clearAllSelectedUrls())}
+                >
+                  Clear All
                 </Button>
-              )}
-            </div>
-          ) : null}
+                &nbsp;
+                {viewingSavedCollection ? null : (
+                  <Button onClick={() => promptAndSaveCollection()}>
+                    Save to My Stuff
+                  </Button>
+                )}
+              </div>
+            ) : null}
+          </ContentHeader>
           <div className="columns">
             <Chart
               sites={currentSites}
@@ -440,23 +331,8 @@ function App() {
         </Content>
       </Layout>
 
-      <h2>site details</h2>
-      <div>
-        <table className="SiteDetails">
-          <thead>
-            <tr>
-              <th>site</th>
-              <th>cdn</th>
-              <th>profile time</th>
-            </tr>
-          </thead>
-          <tbody>
-            {currentSites.map((site) => (
-              <SiteDetail key={site.url} site={site} />
-            ))}
-          </tbody>
-        </table>
-      </div>
+      <SiteDetailsTable sites={currentSites} />
+
       <div className="footer">
         <p>&copy; 2020</p>
       </div>
