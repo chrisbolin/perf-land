79b8526515e66c90b952675e830a0ef50193f251
Paula Lavalle <lavalle.paula@gmail.com>
2020-08-17 20:30:34 -0700

    WIP add react-aria-modal

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,21 +1,35 @@
-import React from "react";
+import React, { FunctionComponent, useState } from "react";
 import styled from "styled-components/macro";
+import Modal from "react-aria-modal";
 
 const Wrapper = styled.div`
   flex: 0 0 ${(props) => props.theme.spacing(30)};
   padding: ${(props) => props.theme.spacing(2)}
     ${(props) => props.theme.spacing(2)} ${(props) => props.theme.spacing(2)}
     ${(props) => props.theme.spacing(4)};
+
+  /* transform: translateX(0%); */
+  border: 1px solid blue;
 `;
 
-const Sidebar = ({
-  children,
-  className,
-}: {
+const MobileWrapper = styled.div``;
+
+const Sidebar: FunctionComponent<{
   children: any;
   className?: string;
-}) => {
-  return <Wrapper className={className}>{children}</Wrapper>;
+}> = ({ children, className }) => {
+  const [isOpen, setOpen] = useState(true);
+
+  const getApplicationNode = () => document.getElementById("root") as Element;
+
+  const handleClose = () => setOpen(false);
+  const handleOpen = () => setOpen(true);
+
+  return (
+    <Modal onExit={handleClose} getApplicationNode={getApplicationNode}>
+      <Wrapper className={className}>{children}</Wrapper>
+    </Modal>
+  );
 };
 
 export default Sidebar;
---
+++ src/react-aria-modal.d.ts
@@ -0,0 +1,6 @@
+import "react-aria-modal";
+
+declare module "react-aria-modal" {
+  const value: any;
+  export default value;
+}
